df19045c7cd0d6bbe2ba9da50b8fe9163e7ae609
rasya_project <srasyakt90@gmail.com>
2026-06-22 21:11:41 +0700

    ketinggalan 1

--- resources/views/admin/logs/index.blade.php
+++ resources/views/admin/logs/index.blade.php
@@ -48,7 +48,7 @@
 
                 <!-- Action Buttons -->
                 <div class="flex gap-2">
-                    <button type="submit" class="flex-1 px-6 py-2.5 bg-slate-850 dark:bg-slate-700 text-white font-bold rounded-xl hover:bg-slate-850 dark:hover:bg-slate-650 transition-all text-sm cursor-pointer shadow-md border border-transparent">
+                    <button type="submit" class="flex-1 px-6 py-2.5 bg-slate-800 dark:bg-slate-700 text-white font-bold rounded-xl hover:bg-slate-700 dark:hover:bg-slate-600 transition-all text-sm cursor-pointer shadow-md border border-transparent">
                         Filter
                     </button>
                     @if(request()->anyFilled(['search', 'action_type', 'role', 'start_date', 'end_date']))
